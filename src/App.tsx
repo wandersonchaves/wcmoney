041c0de5f7a7ec1b363c39d2b4bc0a3ada2f48e8
Wanderson Chaves <wandersonchaves@MacBook-Air-do-Wanderson.local>
2022-07-01 19:50:40 -0300

    criando estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import styled from "styled-components";
+import { GlobalStyle } from "./styles/global";
 
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 64px;
-
-  button: {
-    background: #000;
-  }
-`;
-
-function App() {
+export function App() {
   return (
     <div className="App">
-      <Title>
-        App <button>Salvar</button>
-      </Title>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
-
-export default App;
